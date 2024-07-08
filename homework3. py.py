e2d098e6c2459c7d80aac915a3eb35716467617a
vovan304 <sergiy_kolosov@mail.ru>
2024-07-08 13:51:31 +0300

    Add files via upload

--- homework3. py.py
+++ homework3. py.py
@@ -2,8 +2,7 @@ name = 'Vladimir'
 print ('Name:'+ name)
 age = 56
 print('Age:'+'56')
-new_age = 57
+age = age + 1
 print('New age:'+'57')
-is_student = True
-is_student = 'True'
+is_student = type(True)
 print('Is Student:'+'True')
